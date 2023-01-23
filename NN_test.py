fix gradient, fix backprop

NN_test.py
@@ -1,17 +1,34 @@
-from tools import readTrainMNIST, readTestMNIST, show, label_array
-from tools import lire_alpha_digit, save, load
 from Networks.DNN import DNN
+from tools import readTrainMNIST, readTestMNIST, show_m, label_array
+from tools import save, load, cross_entropie
 import numpy as np
+from time import time
 
-X, Y = readTrainMNIST()
-X_flat, Y_arr = np.reshape(X, (X.shape[0], -1)), label_array(Y)
+X_test, Y_test = readTestMNIST()
+X_test, Y_test = X_test.reshape((X_test.shape[0], 784)), label_array(Y_test)
 
-N_dims = (X_flat.shape[1], 200, 200, 10)
-n_iter_rbm = 100
-n_iter_retro = 200
-eps = 0.1
-tb = 10
+DIMS = [(784, 100, 100, 10), (784, 200, 200, 10), (784, 200, 200, 200, 10),
+        (784, 200, 200, 200, 10), (784, 200, 200, 10),
+        (784, 300, 300, 10), (784, 700, 700, 10)]
 
-NN = DNN(N_dims)
-#NN.pretrain(n_iter_rbm, eps, tb, X_flat)
-#NN.retropropagation(X_flat, Y_arr, eps, tb)
+MODELS_P = []
+MODELS_NP = []
+
+for dim in DIMS:
+    model_name = "DNN-"
+    for N in dim:
+        model_name += "{}x".format(N)
+    model_name = model_name[:-1]
+    model_name += "-pretrained"
+
+    model_p = load(model_name)
+    model_np = DNN(dim)
+
+    MODELS_P.append(model_p)
+    MODELS_NP.append(model_np)
+
+model_p = MODELS_P[0]
+sorties_p = model_p.entree_sortie(X_test)[-1]
+
+model_np = MODELS_NP[0]
+sorties_np = model_np.entree_sortie(X_test)[-1]

Networks/DNN.py
@@ -32,7 +32,7 @@ class DNN:
                 X_work.append(RBM[i].calc_softmax(X_work[i]))
         return X_work
 
-    def retropropagation(self, X, Y, epochs, eps, tb):
+    def retropropagation(self, X, Y, epochs, eps, tb, verbose=False):
         n = X.shape[0]
         N_couches = len(self.DBN.RBM)
         self.trained = True
@@ -54,7 +54,11 @@ class DNN:
                     grad_b = np.sum(C, axis=0)
                     new_DNN.DBN.RBM[p].W -= eps*grad_W/tb
                     new_DNN.DBN.RBM[p].b -= eps*grad_b/tb
-            self.epochs += 1
+                self = deepcopy(new_DNN)
+            self.epochs = self.epochs + 1
+            if verbose:
+                sortie = self.entree_sortie(X)[-1]
+                print(cross_entropie(Y, sortie))
 
     def test(self, X, Y):
         Y_est = self.entree_sortie(X)[-1]

tools.py
@@ -17,13 +17,13 @@ def readTrainMNIST():
     X = np.load('data/m_train_data.npy')
     Y = np.load('data/m_train_label.npy')
 
-    return X, Y
+    return X/255, Y
 
 def readTestMNIST():
     X = np.load('data/m_test_data.npy')
     Y = np.load('data/m_test_label.npy')
 
-    return X, Y
+    return X/255, Y
 
 def label_array(Y):
     if type(Y) == np.uint8:
@@ -45,9 +45,10 @@ def err_quad(X, Y):
 
 def cross_entropie(Y, Y_pred):
     somme = 0
+    n = Y.shape[0]
     for i, j in np.ndindex(Y.shape):
         somme -= Y[i, j]*np.log(Y_pred[i, j])
-    return somme
+    return somme/n
     
 def show(img, titre="default", dim1=20, dim2=16, save=False, filename="foo", show=True):
     image = np.copy(img)